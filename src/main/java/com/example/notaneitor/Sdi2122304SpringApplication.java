62f92f5f2324c72e449b3c9b7a5569eaee6f5dd4
Martin Beltran <uo276244@uniovi.es>
2022-02-11 10:04:33 +0100

    SDI-2122-304-2.1-Create Spring project

---
+++ src/main/java/com/example/notaneitor/Sdi2122304SpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.notaneitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2122304SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2122304SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/notaneitor/Sdi2122304SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notaneitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2122304SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
