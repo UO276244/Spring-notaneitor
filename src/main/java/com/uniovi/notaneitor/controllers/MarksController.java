ea162c9827609c4d131797af433cc7b3132c14d8
Martin Beltran <uo276244@uniovi.es>
2022-02-26 17:00:18 +0100

    SDI-2122-304-4.5-SpringBoot-Busqueda

--- src/main/java/com/uniovi/notaneitor/controllers/MarksController.java
+++ src/main/java/com/uniovi/notaneitor/controllers/MarksController.java
@@ -10,10 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.security.Principal;
@@ -35,7 +32,6 @@ public class MarksController {
 
     @Autowired
     private HttpSession httpSession;
-
 
 
     /**
@@ -124,10 +120,19 @@ public class MarksController {
     }
 
     @RequestMapping("/mark/list")
-    public String getList(Model model, Principal principal) {
+    public String getList(Model model, Principal principal,
+                          @RequestParam(value = "", required = false) String searchText) {
         String dni = principal.getName(); // DNI es el name de la autenticación
         User user = usersService.getUserByDni(dni);
-        model.addAttribute("markList", marksService.getMarksForUser(user));
+
+        if(searchText != null && !searchText.isBlank()){
+            model.addAttribute("markList",
+                    marksService.searchMarksByDescriptionAndNameForUser(searchText,user));
+        }else{
+            model.addAttribute("markList", marksService.getMarksForUser(user));
+        }
+
+
         return "mark/list";
     }
 
